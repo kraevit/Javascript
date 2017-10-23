320c05b1ab372cc4fb3da0f7f47b9b32898528bc
horrow <horrow>
2017-10-23 11:43:30 +0300

    added more files

--- freeCodeCamp/Basic-Algorithm-Scripting/Check-for-Palindromes.js
+++ freeCodeCamp/Basic-Algorithm-Scripting/Check-for-Palindromes.js
@@ -6,7 +6,8 @@ Return true if the given string is a palindrome. Otherwise, return false.
 A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
 
 Note
-You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything lower case in order to check for palindromes.
+You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything lower case in order to
+check for palindromes.
 
 We'll pass strings with varying formats, such as "racecar", "RaceCar", and "race CAR" among others.
 
@@ -23,3 +24,21 @@ String.prototype.toLowerCase()
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
 
 */
+
+function palindrome(str) {
+
+  // defining variables
+  var regEx, newStr, reversedStr;
+  // create regex
+  regEx = /[\W_]/g;
+  // clean the string from unwanted chars
+  newStr = str.toLowerCase().replace(regEx, '');
+  // reverse the string
+  reversedStr = newStr.split('').reverse().join('');
+  // check to see if the given string is a palindrome or not
+  console.log(newStr === reversedStr);
+  // exit the function
+  return newStr === reversedStr;
+}
+
+palindrome("eye");
