42b94971e5d20d04136e078a579ae97036555bef
Krasimir Kraev <kraevit@gmail.com>
2018-11-17 10:06:36 +0200

    fixed indentation on dont do js file.

--- js-oop/prototype-chain-dont-do-this.js
+++ js-oop/prototype-chain-dont-do-this.js
@@ -11,16 +11,14 @@
     }
   };
 
-var john = {
-  firstname: 'Joe',
-  lastname: 'Doe'
-};
+	var john = {
+		firstname: 'Joe',
+		lastname: 'Doe'
+	};
 
-// don't do this EVER! For demo purposes only!!! Otherwise will cause PERFORMANCE issue!
-john.__proto__ = person;
+		// don't do this EVER! For demo purposes only!!! Otherwise will cause PERFORMANCE issue!
+		john.__proto__ = person;
 
-console.log( john.getFullName() ); // here we have access to getFullname() of person obj, because of linked protos
-console.log( john.firstname ); // here we have acces only to john obj firstname prop, because it overwrites person obj firtname prop
-
-
+	console.log( john.getFullName() ); // here we have access to getFullname() of person obj, because of linked protos
+	console.log( john.firstname ); // here we have acces only to john obj firstname prop, because it overwrites person obj firtname prop
 })( window );
