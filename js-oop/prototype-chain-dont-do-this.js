2947acb9739891f58f5dd9065156044c7f215e74
Krasimir Kraev <kraevit@gmail.com>
2018-11-17 10:14:10 +0200

    proto update.

--- js-oop/prototype-chain-dont-do-this.js
+++ js-oop/prototype-chain-dont-do-this.js
@@ -1,24 +1,28 @@
-(function IIFE( global ) {
-	"use strict";
+(function IIFE() {
 
-  // PROTOTYPE CHAIN
-
+  // Prototype Chain - DON'T do this!
+  
   var person = {
     firstname: 'Default',
-    lastname: 'Default',
+    lastname:  'Default',
     getFullName: function() {
       return this.firstname + ' ' + this.lastname;
     }
   };
-
-	var john = {
-		firstname: 'Joe',
-		lastname: 'Doe'
-	};
-
-		// don't do this EVER! For demo purposes only!!! Otherwise will cause PERFORMANCE issue!
-		john.__proto__ = person;
-
-	console.log( john.getFullName() ); // here we have access to getFullname() of person obj, because of linked protos
-	console.log( john.firstname ); // here we have acces only to john obj firstname prop, because it overwrites person obj firtname prop
-})( window );
+  
+  var john = {
+    firstname: 'John',
+    lastname: 'Doe'
+  }
+  
+  // DON'T DO THIS EVER! For demo purposes ONLY!!!
+  // Otherwise it will cause PERFORMANCE issue!!!!!!!
+  john.__proto__ = person;
+  
+  // here we have access to getFullName() method of person obj, because of linked prostotypes.
+  console.log( john.getFullName() );
+  
+  // here we have access only to john's firstname prop, because it overwrites firstname prop of the person obj.
+  console.log( john.firstname ); 
+    
+})();
