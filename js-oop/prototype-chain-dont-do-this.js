52995175fa8d2ad58a0c7386cc33e2d0c03365ef
Krasimir Kraev <kraevit@gmail.com>
2018-11-17 10:05:28 +0200

    fixed indentation on don't do js file.

--- js-oop/prototype-chain-dont-do-this.js
+++ js-oop/prototype-chain-dont-do-this.js
@@ -1,30 +1,26 @@
-			(function IIFE( global ) {	
-				"use strict";
+(function IIFE( global ) {	
+	"use strict";
 
-				// PROTOTYPE CHAIN
+  // PROTOTYPE CHAIN
 
-				var person = {
+  var person = {
+    firstname: 'Default',
+    lastname: 'Default',
+    getFullName: function() {
+      return this.firstname + ' ' + this.lastname;
+    }
+  };
 
-					firstname: 'Default',
-					lastname: 'Default',
-					getFullName: function() {
-						return this.firstname + ' ' + this.lastname;
-					} 
+var john = {
+  firstname: 'Joe',
+  lastname: 'Doe'
+};
 
-				}
-				
-				var john = {
+// don't do this EVER! For demo purposes only!!! Otherwise will cause PERFORMANCE issue!
+john.__proto__ = person;
 
-					firstname: 'Joe',
-					lastname: 'Doe'
-
-				}
-
-				// don't do this EVER! For demo purposes only!!! Otherwise will cause PERFORMANCE issue!
-				john.__proto__ = person;
-				
-				console.log(john.getFullName()); // here we have access to getFullname() of person obj, because of linked protos
-				console.log(john.firstname); // here we have acces only to john obj firstname prop, because it overwrites person obj firtname prop
+console.log( john.getFullName() ); // here we have access to getFullname() of person obj, because of linked protos
+console.log( john.firstname ); // here we have acces only to john obj firstname prop, because it overwrites person obj firtname prop
 
 
-			})( window );
+})( window );
