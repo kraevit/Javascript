e7052b39ff96bce81695e005f2de19bbc6c97bc2
Krasimir Kraev <kraevit@gmail.com>
2018-11-19 00:20:12 +0200

    Update for-loop.js

--- js-closures/for-loop.js
+++ js-closures/for-loop.js
@@ -1,5 +1,45 @@
 for (var i=0; i<=5; i++) {
-  setTimeout( function timer() {
+  setTimeout( function timer(){
     console.log( i ); // 6 6 6 6 6
   }, i*1000 );
+}
+
+for (var i=0; i<=5; i++) {
+  (function() {
+    setTimeout( function timer(){
+      console.log( i ); // 6 6 6 6 6
+    }, i*1000 );
+  })();
+}
+
+for (var i=0; i<=5; i++) {
+  (function () {
+    var j = i;
+    setTimeout( function timer(){
+      console.log( j ); // 1 2 3 4 5
+    }, j*1000);
+  })();
+}
+
+for (var i=0; i<=5; i++) {
+  (function ( j ){
+    setTimeout( function timer(){
+      console.log( j ); // 1 2 3 4 5
+    });
+  })( i );
+}
+
+// let
+
+for (var i=0; i<=5; i++) {
+  let j = i;
+  setTimeout( function timer(){
+    console.log( j ); // 1 2 3 4 5
+  }, j*1000);
+}
+
+for (let i=0; i<=5; i++) {
+  setTimeout( function timer(){
+    console.log( i );  // 1 2 3 4 5
+  }, i*1000);
 }
