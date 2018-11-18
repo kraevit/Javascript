94f491517312c4e457a5f96d508e733346664c3a
Krasimir Kraev <kraevit@gmail.com>
2018-11-19 00:34:43 +0200

    Update for-loop.js

--- js-closures/for-loop.js
+++ js-closures/for-loop.js
@@ -23,7 +23,7 @@ for (var i=0; i<=5; i++) {
   (function () {
     var j = i;
     setTimeout( function timer(){
-      console.log( j ); // 1 2 3 4 5
+      console.log( j ); // 0 1 2 3 4 5
     }, j*1000);
   })();
 }
@@ -32,8 +32,8 @@ for (var i=0; i<=5; i++) {
 for (var i=0; i<=5; i++) {
   (function ( j ){
     setTimeout( function timer(){
-      console.log( j ); // 1 2 3 4 5
-    });
+      console.log( j ); // 0 1 2 3 4 5
+    }, j*1000);
   })( i );
 }
 /* some prefer this */
@@ -50,14 +50,14 @@ It essentially turns a block into a scope that we can close over. So, the follow
 for (var i=0; i<=5; i++) {
   let j = i;
   setTimeout( function timer(){
-    console.log( j ); // 1 2 3 4 5
+    console.log( j ); // 0 1 2 3 4 5
   }, j*1000);
 }
 
 
 for (let i=0; i<=5; i++) {
   setTimeout( function timer(){
-    console.log( i );  // 1 2 3 4 5
+    console.log( i );  // 0 1 2 3 4 5
   }, i*1000);
 }
 // let shines!
