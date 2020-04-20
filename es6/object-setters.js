9fbbca8f4a46ed990361ee15c371cafd60aa10c5
K.Kraev <kraevit@gmail.com>
2020-04-20 10:55:46 +0300

    Fix object-setter.js

--- es6/object-setters.js
+++ es6/object-setters.js
@@ -15,9 +15,9 @@ const robot = {
    } else {
    	console.log('Pass in a number that is greater than or equal to 0');  
    } */ 
-   typeof num== 'number' && num>= 0?
-   this._numOfSensors = num :
-   console.log('Pass in a number that is greater than or equal to 0');
+   typeof num === 'number' && num >= 0 ?
+    this._numOfSensors = num :
+    console.log('Pass in a number that is greater than or equal to 0');
  }
 };
 
